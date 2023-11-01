79312c3830f85b84b01861aebe820f3df3d36a2b
Victor <vsantos.part@gmail.com>
2023-11-01 16:46:30 -0300

    feat: criação do service de transação

--- src/main/java/com/picpaySimplificado/domain/transaction/Transaction.java
+++ src/main/java/com/picpaySimplificado/domain/transaction/Transaction.java
@@ -4,17 +4,15 @@ import com.picpaySimplificado.domain.user.User;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Entity(name="transactions")
 @Table(name="transactions")
-@Getter
-@Setter
 @AllArgsConstructor
+@NoArgsConstructor
 @EqualsAndHashCode(of="id")
 public class Transaction {
     @Id
@@ -29,4 +27,43 @@ public class Transaction {
     private User receiver;
     private LocalDateTime timestamp;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public User getSender() {
+        return sender;
+    }
+
+    public void setSender(User sender) {
+        this.sender = sender;
+    }
+
+    public User getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(User receiver) {
+        this.receiver = receiver;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
 }
