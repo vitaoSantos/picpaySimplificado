2d024854f95f7c517828a17c9812a91c62f26063
Victor <vsantos.part@gmail.com>
2023-11-01 16:05:26 -0300

    feat: atualização user entity

--- src/main/java/com/picpaySimplificado/domain/user/User.java
+++ src/main/java/com/picpaySimplificado/domain/user/User.java
@@ -3,31 +3,89 @@ package com.picpaySimplificado.domain.user;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
-
 
 import java.math.BigDecimal;
 
-@Entity(name="users")
-@Table(name="users")
-@Getter
-@Setter
+@Entity(name = "users")
+@Table(name = "users")
 @AllArgsConstructor
-@EqualsAndHashCode(of="id")
+@EqualsAndHashCode(of = "id")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String firstName;
     private String lastName;
-    @Column(unique=true)
+    @Column(unique = true)
     private String document;
-    @Column(unique=true)
+    @Column(unique = true)
     private String email;
     private String password;
     private BigDecimal balance;
     @Enumerated(EnumType.STRING)
     private UserType userType;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDocument() {
+        return document;
+    }
+
+    public void setDocument(String document) {
+        this.document = document;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    public UserType getUserType() {
+        return userType;
+    }
+
+    public void setUserType(UserType userType) {
+        this.userType = userType;
+    }
 }
